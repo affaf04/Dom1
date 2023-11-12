6b90aa5e508739b25f24ab6e2526807ffa0e5bb8
affaf04 <affaftaouti@gmail.com>
2023-11-12 18:42:51 +0100

    edit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+const mainEl = document.querySelector("main");
+
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+
+topMenuEl.style.height = "100%";
+
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+topMenuEl.classList.add("flex-around");
+
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+menuLinks.forEach((link) => {
+  link.document.createElement("a");
+  link.setAttribute("href", link.href);
+  topMenuEl.appendChild(link);
+});
